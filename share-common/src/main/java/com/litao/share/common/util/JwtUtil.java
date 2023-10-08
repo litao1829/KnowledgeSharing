373b7342a40fde6176bf2d096b788a1675c2b0a2
litao <2532566074@qq.com>
2023-10-08 13:57:47 +0800

    使用 IDEA 自带的 HttpClient 记住登录信息

--- share-common/src/main/java/com/litao/share/common/util/JwtUtil.java
+++ share-common/src/main/java/com/litao/share/common/util/JwtUtil.java
@@ -26,7 +26,7 @@ public class JwtUtil {
 
     public static String createToken(Long id, String phone) {
         DateTime now = DateTime.now();
-        DateTime expTime = now.offsetNew(DateField.SECOND, 100000);
+        DateTime expTime = now.offsetNew(DateField.HOUR, 48);
         Map<String, Object> payload = new HashMap<>();
         // 签发时间
         payload.put(JWTPayload.ISSUED_AT, now);
